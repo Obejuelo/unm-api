f525f65e785ae081ad242a9ce32c3616b15f069c
Obed Hernandez <obejuela4@gmail.com>
2019-06-05 12:15:13 -0500

    mongo var

--- config/config.js
+++ config/config.js
@@ -4,11 +4,6 @@
 process.env.PORT = process.env.PORT || 5000;
 
 //====================
-// =====DATABASE======
-//====================
-process.env.URLDB = 'mongodb://localhost:27017/bd_unm';
-
-//====================
 // =======seed========
 //====================
 process.env.SEED = process.env.SEED || 'seed-desarrollo';
@@ -17,3 +12,20 @@ process.env.SEED = process.env.SEED || 'seed-desarrollo';
 // ==Caducidad token==
 //====================
 process.env.CAD_TOKEN = '24h';
+
+// ============================
+//  Entorno
+// ============================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// ============================
+//  Base de datos
+// ============================
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/bd_unm';
+} else {
+    urlDB = process.env.MONGO_URI;
+}
+process.env.URLDB = urlDB;
